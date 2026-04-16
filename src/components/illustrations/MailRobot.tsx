5db09773f3d7e65c928c229e10fe3285824d1668
tulinleo <leonid.tulin0@gmail.com>
2026-04-16 15:10:20 +0200

    fix: move mail robot back to right side

--- src/components/illustrations/MailRobot.tsx
+++ src/components/illustrations/MailRobot.tsx
@@ -6,7 +6,7 @@ export default function MailRobot() {
         viewBox="0 0 100 140"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        className="absolute left-6 bottom-6 w-[120px] pointer-events-none"
+        className="absolute right-24 bottom-6 w-[120px] pointer-events-none"
       >
         {/* Body */}
         <rect x="20" y="45" width="60" height="50" rx="8"
